7a3120b152e35339c994638a9d79ffada8e6e751
GitbuddySharry <96821148+GitbuddySharry@users.noreply.github.com>
2021-12-31 00:10:25 +0530

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,5 +1,7 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
+st.title("hey Guys find out your BMI !!")
 df=pd.read_csv("/content/500_Person_Gender_Height_Weight_Index.csv")
 x=df.iloc[:,[1,2]].values
 df=df.replace({'Index' : { 0 : "Extremely_Weak", 1 : "Weak", 2 : "Normal",3:"Overweight",4:"Obesity",5:"Extreme_Obesity" }})
@@ -13,6 +15,7 @@ height=st.slider("Height",float(xmin[0]),float(xmax[0]))
 weight=st.slider("Weight",float(xmin[1]),float(xmax[1]))
 
 y_pred=model.predict([[height,weight]])
+print ("Your Body mass Index is :")
 
 op=["Extremely_Weak","Weak","Normal","Overweight","Obesity","Extreme_Obesity"]
 st.title(op[y_pred[0]])
