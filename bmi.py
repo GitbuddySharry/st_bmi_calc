785a5478208b20d72efa6ebd93dfd4e3286e74c6
GitbuddySharry <96821148+GitbuddySharry@users.noreply.github.com>
2021-12-31 00:00:28 +0530

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+df=pd.read_csv("/content/500_Person_Gender_Height_Weight_Index.csv")
+x=df.iloc[:,[1,2]].values
+df=df.replace({'Index' : { 0 : "Extremely_Weak", 1 : "Weak", 2 : "Normal",3:"Overweight",4:"Obesity",5:"Extreme_Obesity" }})
+y=df.iloc[:,-1].values
+from sklearn.neighbors import KNeighborsClassifier
+model=KNeighborsClassifier(n_neighbors=23,metric="euclidean")
+model.fit(x,y)
+xmin=np.min(x,axis=0)
+xmax=np.max(x,axis=0)
+height=st.slider("Height",float(xmin[0]),float(xmax[0]))
+weight=st.slider("Weight",float(xmin[1]),float(xmax[1]))
+
+y_pred=model.predict([[height,weight]])
+
+op=["Extremely_Weak","Weak","Normal","Overweight","Obesity","Extreme_Obesity"]
+st.title(op[y_pred[0]])
